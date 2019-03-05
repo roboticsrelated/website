d163e43b59f321062dd6fd6a7673b7ed3974be92
Mitch Dull <mdull3195@gmail.com>
2019-03-05 18:57:33 -0500

    Cleaned up article card component

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,50 +1,33 @@
-import React from 'react';
+import React from 'react'
 import { navigate } from 'gatsby'
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import styles from './styles/ArticleCard.jss'
 
-const styles = {
-  card: {
-    maxWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-};
-
-function SmallCard(props) {
-  const { classes, data } = props;
-  console.log(data);
+function ArticleCard(props) {
+  const { classes, title, author, date, path } = props;
 
   return (
     <Card className={classes.card}>
       <CardContent>
-        <Typography variant="title">
-          {data.title}
+        <Typography variant="h4">
+          {title}
         </Typography>
-        <Typography variant="subtitle1">
-          {data.author}
+        <Typography variant="h5">
+          {author}
         </Typography>
-        <Typography variant="subtitle2">
-          {data.date}
+        <Typography variant="h6">
+          {date}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" onClick={() => navigate(data.path)}>
-          <Typography variant="subtitle2">
+        <Button size="small" onClick={() => navigate(path)}>
+          <Typography variant="h6">
             Read More
           </Typography>
         </Button>
@@ -53,9 +36,12 @@ function SmallCard(props) {
   );
 }
 
-SmallCard.propTypes = {
-  classes: PropTypes.object.isRequired,
-  data: PropTypes.object.isRequired,
+ArticleCard.propTypes = {
+  classes : PropTypes.object.isRequired,
+  title : PropTypes.string.isRequired,
+  author : PropTypes.string,
+  date : PropTypes.string,
+  path : PropTypes.string,
 };
 
-export default withStyles(styles)(SmallCard);
+export default withStyles(styles)(ArticleCard);
